183f6edebe3140c3e6bd2282b5e5664fbd398858
Mario Näther <mario-naether@gmx.de>
2018-01-28 13:14:08 +0100

    - set typo3 dependencies to v8

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.8',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.99.99',
+			'typo3' => '6.2.0-8.99.99',
 		),
 		'conflicts' => array(
 		),
